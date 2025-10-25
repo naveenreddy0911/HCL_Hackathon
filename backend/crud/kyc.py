246fe3b29310c845b5e30c7453b0ef88b0484824
naveenreddy0911 <naveenreddy0911@email.com>
2025-10-25 15:21:55 +0530

    feat: store kyc documents as large binary files (kyc-documents table)

--- backend/crud/kyc.py
+++ backend/crud/kyc.py
@@ -1,12 +1,20 @@
 from sqlalchemy.orm import Session
 from backend.models.user import User
+from backend.models.kyc_documents import KYC_Documents
 from fastapi import UploadFile
 
 def upload_kyc_documents(db: Session, id: int, aadhaar: UploadFile, pan: UploadFile) -> bool:
     db_user = db.query(User).filter(User.id == id).first()
     if not db_user:
         return False
-    db_user.kyc_status = 'in_progress' # Simulate upload process
+    
+    kyc_doc = KYC_Documents(user_id=id)
+    db.add(kyc_doc)
+        
+    kyc_doc.aadhaar = aadhaar.file.read()
+    kyc_doc.pan = pan.file.read()
+    
+    db_user.kyc_status = 'in_progress'
     db.commit()
     return True
 
--- backend/routes/kyc_upload.py
+++ backend/routes/kyc_upload.py
@@ -3,15 +3,22 @@ from backend.schemas.user import UserResponse
 from backend.auth import get_current_user
 from sqlalchemy.orm import Session
 from backend.db import get_db
-from backend.crud.kyc import upload_kyc_documents
+from backend.crud.kyc import check_kyc_status, upload_kyc_documents
 
 router = APIRouter()
 
 @router.post("/kyc-upload")
-def kyc_upload(aadhaar: UploadFile = File(...), pan: UploadFile = File(...), user: UserResponse = Depends(get_current_user),
-               db: Session = Depends(get_db)):
+def kyc_upload(
+    aadhaar: UploadFile = File(...),
+    pan: UploadFile = File(...),
+    user: UserResponse = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
     if not user:
         raise HTTPException(status_code=401, detail="Not authenticated")
+    
+    if check_kyc_status(db, user.id) == 'completed':
+        raise HTTPException(status_code=400, detail="KYC already completed")
     
     if not upload_kyc_documents(db, user.id, aadhaar, pan):
         raise HTTPException(status=500, detail="Failed to upload KYC documents")
