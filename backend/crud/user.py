0d0f4910293b5d4cc1a0a893fd0c0de625a483ac
naveenreddy0911 <naveenreddy0911@email.com>
2025-10-25 13:14:31 +0530

    feat: password hashing

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -1,9 +1,13 @@
 from sqlalchemy.orm import Session
 from backend.models.user import User
 from backend.schemas.user import UserSignin, UserSignup
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["argon2"])
 
 def create_user(db: Session, user: UserSignup):
-    db_user = User(name=user.name, email=user.email, password=user.password)
+    hashed_password = pwd_context.hash(user.password)
+    db_user = User(name=user.name,email=user.email,password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -11,7 +15,7 @@ def create_user(db: Session, user: UserSignup):
 
 def authenticate_user(db: Session, user: UserSignin):
     db_user = db.query(User).filter(User.email == user.email).first()
-    if not db_user or db_user.password!=user.password:
+    if not db_user or not pwd_context.verify(user.password, db_user.password):
         return False
     return db_user
 
