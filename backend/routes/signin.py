8406f7ea3da2a6266383039cc0639ae27f36361c
naveenreddy0911 <naveenreddy0911@gmail.com>
2025-10-25 13:58:46 +0530

    feat: jwt authentication

--- backend/routes/signin.py
+++ backend/routes/signin.py
@@ -3,6 +3,7 @@ from backend.crud.user import authenticate_user
 from backend.schemas.user import UserSignin
 from sqlalchemy.orm import Session
 from backend.db import get_db
+from backend.auth import create_access_token
 
 router = APIRouter()
 
@@ -12,6 +13,7 @@ def signin(user: UserSignin, db: Session = Depends(get_db)):
     if not db_user:
         raise HTTPException(status_code=401, detail="Email or password didn't match")
     
+    access_token = create_access_token(data={"user_id": db_user.id})
     return {
-        "message": "Your are now signed in"
+        "access_token": access_token
     }
