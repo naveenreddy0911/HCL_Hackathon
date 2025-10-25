a818ead23c95d44d653d31ef2d1a3ccc8ab25bd3
naveenreddy0911 <naveenreddy0911@gmail.com>
2025-10-25 16:41:25 +0530

    fix: pin number for checking balance

--- backend/routes/check_account_balance.py
+++ backend/routes/check_account_balance.py
@@ -8,13 +8,16 @@ from backend.db import get_db
 router = APIRouter()
 
 @router.post("/check-account-balance")
-def signin(account_number: str, user: UserResponse = Depends(get_current_user),db: Session = Depends(get_db)):
+def signin(account_number: str, pin: int, user: UserResponse = Depends(get_current_user),db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=401, detail="Not authenticated")
     
     account = get_account_by_number(db, account_number)
     if not account:
         raise HTTPException(status_code=404, detail="Account not found")
+    
+    if account.pin != pin:
+        raise HTTPException(status_code=403, detail="Invalid PIN")
     
     return {
         f"Your account balance is {account.balance}"
